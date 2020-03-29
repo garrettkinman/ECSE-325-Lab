be8ad61d14e02a2e563a5e6a9bafc596aa774603
garrettkinman <garrettkinman@gmail.com>
2020-03-29 16:48:05 -0400

    declared two more functions

--- Lab 2/bits.py
+++ Lab 2/bits.py
@@ -12,14 +12,26 @@ def read_numbers(filename):
     file.close()
     return numbers
 
+# function that takes in a float and returns the minimum number of bits required to represent it
+def find_min_bits_float(num):
+    # TODO
 
+    return
+
+def find_min_bits_int(num):
+    # TODO
+    
+    return
 
 # ~~~ PROGRAM STARTS HERE ~~~
 
 input_x_file = "ThuFrilab2-x.txt"
 input_y_file = "ThuFrilab2-y.txt"
 
+# read in the inputs
 input_x = read_numbers(input_x_file)
 input_y = read_numbers(input_y_file)
 
+# test
 print(input_x[0:10])
+print(input_y[0:10])
